45c1b02c93be5d6d510597ec49cd3e1e867497e2
TomiCornejo <tomas.cornejo.z@mail.pucv.cl>
2022-05-08 11:49:58 -0400

    foro listo

---
+++ gota-a-gota/src/app/models/post.model.ts
@@ -0,0 +1,10 @@
+export class Post{
+    titulo:string;
+    descripcion:string;
+    // imagen:string;
+
+    constructor(titulo:string,descripcion:string){
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+    }
+}
